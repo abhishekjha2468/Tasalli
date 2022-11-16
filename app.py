76e3bfce95aab0eb62ae81afed38a2be63c44742
ABHISHEK JHA <abhishekjha2468@gmail.com>
2022-11-17 01:10:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ from flask_cors import CORS
 import json
 
 app = Flask(__name__,static_url_path='', 
-            static_folder='web/static',
-            template_folder='web/templates')
-app.config["DEBUG"] = True
+            static_folder='static',
+            template_folder='templates')
+# app.config["DEBUG"] = True
 # app._static_folder = os.path.abspath("templates/")
 CORS(app)
 #run_with_ngrok(app) 
